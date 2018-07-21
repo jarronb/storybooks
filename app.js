2f673b1ba2dbbf3bf62e78f062940242a2c703f7
Jarronb <baileyjarron@gmail.com>
2018-07-21 17:21:02 -0500

    google auth frame

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+//PASSPORT CONFIG 
+require('./config/passport')(passport);
+
+//LOAD ROUTES
+const auth = require('./routes/auth');
 
 
 // APP INDEX PAGE
 app.get('/', (req,res) => {
   res.send(`It works`);
 });
+
+// USE ROUTES
+app.use('/auth', auth);
 
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`);
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  monogoURI: 'mongodb://jarron:Killaj12!@ds147011.mlab.com:47011/storybooks-devjb',
+  googleClientID: '764408142147-fnp4e3327cfuo8eh2mkcgokjbrvtptba.apps.googleusercontent.com',
+  googleClientSecret: '9eOMlA8wtBg3LiH3AnGlpFv1'
+}
